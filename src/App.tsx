088ce462624208bcf091aeebca7ec3470519942e
Veda Vyas Prabu <vyas365@vedas-air.home>
2023-12-20 22:38:41 -0500

    start afresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
 import * as React from "react";
-import NavBar from "./components/others/NavBar";
-import Todo from "./components/todos/Todos";
-function App() {
-  const items = ["Home", "About", "Contact Us"];
 
-  return (
-    <>
-      <NavBar items={items} style="Gold" />
-      <Todo />
-    </>
-  );
+function App() {
+  return <></>;
 }
 
 export default App;
